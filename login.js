494f2df41a68366bd380332ae06024cce48e4658
ayyyy77777 <136596245+ayyyy77777@users.noreply.github.com>
2023-07-05 15:29:42 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+const correctEmail = "ayy@gmail.com";
+const correctPassword = "060704";
+
+const messageContainer = document.getElementById("message-container");
+
+
+function ceklogin(){
+    const userEmail = document.getElementById('fEmail').
+    value;
+    const userPassword = document.getElementById('fPassword').
+    value;
+
+    if (userEmail != correctEmail || userPassword != 
+        correctPassword) {
+            messageContainer.innerHTML = "<p class='error-message'>Login Gagal, Email atau Password salah!!!</p>";
+        } else {
+            messageContainer.innerHTML = "<p class='success-message'>Berhasil Login . . .!</p>";
+        }
+    
+}
